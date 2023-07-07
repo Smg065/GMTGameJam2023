b7c10d497de6915488f4c9fe3992cce1a7827df3
Smg065 <tails2tails@gmail.com>
2023-07-07 17:23:38 -0600

    DoorLogic

--- GMTGameJam2023/Assets/NavigationLogic.cs
+++ GMTGameJam2023/Assets/NavigationLogic.cs
@@ -7,7 +7,9 @@ public class NavigationLogic : MonoBehaviour
 {
     public NavMeshAgent agent;
     public Transform goalTransform;
+    public Vector3 currentGoalPos;
     public bool debugCheckGoalPath;
+    public LayerMask doorMask;
     private void Start()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -15,12 +17,24 @@ public class NavigationLogic : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(goalTransform.position);
+        if (debugCheckGoalPath) CheckGoalPath(goalTransform.position);
+        agent.SetDestination(currentGoalPos);
     }
     void CheckGoalPath(Vector3 goalPosition)
     {
         NavMeshPath pathToGoal = new NavMeshPath();
-        NavMesh.CalculatePath(transform.position, goalPosition, 0, pathToGoal);
+        NavMesh.CalculatePath(transform.position, goalPosition, 1, pathToGoal);
         Vector3[] goalCorners = pathToGoal.corners;
+        Debug.Log(pathToGoal.corners.Length);
+        for (int eachCorner = 1; eachCorner < goalCorners.Length; eachCorner++)
+        {
+            if (Physics.Linecast(goalCorners[eachCorner - 1] + Vector3.up, goalCorners[eachCorner] + Vector3.up, out RaycastHit foundDoor, doorMask, QueryTriggerInteraction.Ignore))
+            {
+                Debug.Log("Door Found!");
+                currentGoalPos = foundDoor.point - ((goalCorners[eachCorner] - goalCorners[eachCorner - 1]).normalized * 1.5f);
+                return;
+            }
+        }
+        currentGoalPos = goalTransform.position;
     }
 }
