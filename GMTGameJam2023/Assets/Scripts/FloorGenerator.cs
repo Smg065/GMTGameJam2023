9d564c9d6fb55c4f4c874b8ea2808873733ca997
NekoNoka <50730918+NekoNoka@users.noreply.github.com>
2023-07-07 15:42:01 -0700

    celled floors

--- GMTGameJam2023/Assets/Scripts/FloorGenerator.cs
+++ GMTGameJam2023/Assets/Scripts/FloorGenerator.cs
@@ -19,21 +19,29 @@ public class FloorGenerator : MonoBehaviour
     public Transform[] floors;
     public Transform[] stairs;
     public static int[] wallHeights = { };
-    public static int[] floorHeights = { 0, 4, 8 };
+    public static int[] floorHeights = { 4, 8 };
     public static int[] doorHeights = {  };
     public static int[] stairHeights = {  };
+    public Transform floor;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < floors.Length; i++)
+        floor = Instantiate(floorCellPrefab, new Vector3(0, 0, 0), Quaternion.identity).transform;
+        floor.localScale = new Vector3(56, 1, 56);
+        for (int x = -3; x < 3; x++)
         {
-            floors[i] = Instantiate(floorPrefab, new Vector3(0, floorHeights[i], 0), Quaternion.identity).transform;
-            floors[i].localScale = new Vector3(50, 1, 50);
+            for (int z = -3; z < 3; z++)
+            {
+                for (int y = 0; y < floorHeights.Length; y++)
+                {
+                    floors[y] = Instantiate(floorCellPrefab, new Vector3(x * 8, floorHeights[y], z * 8), Quaternion.identity).transform;
+                }
+            }
         }
         for (int i = 0; i < doors.Length; i++)
         {
-            doors[i] = Instantiate(doorwayPrefab, new Vector3(i*2, 0, 0), Quaternion.identity).transform;
+            //doors[i] = Instantiate(doorwayPrefab, new Vector3(i*2, 0, 0), Quaternion.identity).transform;
         }
         //doors[0].position = new Vector3(0, 0, 0);
     }
