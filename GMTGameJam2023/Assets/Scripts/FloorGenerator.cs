02e90ddd75cbc18493bab05ec6ef9b670d81e6b8
Smg065 <tails2tails@gmail.com>
2023-07-07 12:23:27 -0600

    Basic Setup

---
+++ GMTGameJam2023/Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour
+{
+    public Vector2 floorSize;
+    public int totalFloors;
+    public GameObject doorwayPrefab;
+    public GameObject wallPrefab;
+    public GameObject floorPrefab;
+    public GameObject ceilingPrefab;
+    public GameObject stairsPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
