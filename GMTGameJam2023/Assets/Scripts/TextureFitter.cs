19494143d922ba5b3f07226961c3815af345bd33
Smg065 <tails2tails@gmail.com>
2023-07-08 17:08:35 -0600

    WalltypeUpdate

--- GMTGameJam2023/Assets/Scripts/TextureFitter.cs
+++ GMTGameJam2023/Assets/Scripts/TextureFitter.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class TextureFitter : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Vector2 scaleMulti;
     void Start()
     {
         Material targetMaterial = GetComponent<MeshRenderer>().material;
-        targetMaterial.mainTextureScale = new Vector2(transform.lossyScale.x, transform.lossyScale.y);
+        targetMaterial.mainTextureScale = new Vector2(transform.lossyScale.x * scaleMulti.x, transform.lossyScale.y * scaleMulti.y);
     }
 }
