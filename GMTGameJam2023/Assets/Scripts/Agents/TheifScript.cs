9cf9b5773d557adc900847eeff0b394304fa4efc
NekoNoka <50730918+NekoNoka@users.noreply.github.com>
2023-07-08 09:29:05 -0700

    added thief to test emily

--- GMTGameJam2023/Assets/Scripts/Agents/TheifScript.cs
+++ GMTGameJam2023/Assets/Scripts/Agents/TheifScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class TheifScript : NavigationLogic
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public int thiefMode;
+    public bool searchOverride;
+    public Transform goalTarget;
+    public Transform exitTarget;
+    public bool patrolFirstPoint;
+    public float waitTime;
     new void Update()
     {
+        if (CloseEnough(goalTransform.position, 1.5f))
+        {
+            //Wait for 3 seconds
+            waitTime -= Time.deltaTime;
+            if (waitTime <= 0)
+            {
+                waitTime += Random.Range(2, 4);
+                //Once you're done waiting, move to the other point
+                if (patrolFirstPoint) goalTransform.position = goalTarget.position;
+                else goalTransform.position = exitTarget.position;
+                patrolFirstPoint = !patrolFirstPoint;
+                CheckGoalPath(goalTransform.position);
+            }
+        }
         base.Update();
     }
 }
